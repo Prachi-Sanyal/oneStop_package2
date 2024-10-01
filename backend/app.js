6e74f17002b738bd7ad35d909645153836533157
Prachi-Sanyal <161684030+Prachi-Sanyal@users.noreply.github.com>
2024-10-01 09:28:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
+// app.js
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const connectDB = require('./db/connection');
 const packageRoutes = require('./routes/packageRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+
+dotenv.config({ path: './config/.env' });
 
 const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
+app.use('/api/payment', paymentRoutes);
 app.use('/api/packages', packageRoutes);
 app.use('/api/reviews', reviewRoutes);
 
